10e5482340ccf70349ac679d6a764d672848e5ce
Félix Cloutier <felix@70-118-111-187.res.bhn.net>
2012-10-20 21:42:17 -0400

    Initial Commit

---
+++ pefdump/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pefdump
+//
+//  Created by Félix on 2012-10-20.
+//  Copyright (c) 2012 Félix. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
