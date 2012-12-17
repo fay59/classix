89c35a9a3e79861dfb1041bfcb99cff796713943
Félix Cloutier <felix@felixcloutier.com>
2012-12-17 01:48:21 -0500

    Renamed CXDocument to CXDebugUI

--- Classix Debugger/CXDocument.mm
+++ Classix Debugger/CXDocument.mm
@@ -90,7 +90,7 @@ static NSImage* functionImage;
 {
 	// Override returning the nib file name of the document
 	// If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
-	return @"CXDocument";
+	return @"CXDebugUI";
 }
 
 -(void)windowControllerDidLoadNib:(NSWindowController *)aController
